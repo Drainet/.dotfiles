60d071ba25524188c857d3430211aaa44762c953
drain <drain.cy.chang@gmail.com>
2022-10-15 17:06:40 +0800

    use pure lua nvim config

---
+++ .config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,10 @@
+require('telescope').setup({
+  defaults = {
+    layout_config = {
+      horizontal = { preview_width = 0.7 }
+      -- other layout configuration here
+    },
+    -- other defaults configuration here
+  },
+  -- other configuration values here
+})
---
+++ .config/nvim/lua/drain/init.lua
@@ -0,0 +1,2 @@
+require("drain.set")
+require("drain.remap")
